9de5801f35a269eaab72f585d951a9f8d6037c47
Hosein jafarnejad <hoseinjafarnejad@hotmail.com>
2019-05-28 00:02:05 +0430

    Basic methods for link service is done. PSQL scripts changed (default value added for creation_date fields)

---
+++ src/main/java/com/hosmos/linkind/controller/LinksController.java
@@ -0,0 +1,55 @@
+package com.hosmos.linkind.controller;
+
+import com.hosmos.linkind.models.Link;
+import com.hosmos.linkind.services.LinkService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/links")
+public class LinksController {
+
+    private LinkService linkService;
+
+    @Autowired
+    public void setLinkService(LinkService linkService) {
+        this.linkService = linkService;
+    }
+
+    @PostMapping
+    public ResponseEntity saveLink (@RequestParam("address") String address) {
+        linkService.save(address);
+
+        return ResponseEntity.ok().body(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteLink (@PathVariable(name = "id") int id) {
+        linkService.delete(id);
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Link getLink (@PathVariable(name = "id") int id) {
+        return linkService.get(id);
+    }
+
+    @GetMapping(value = "/address/{shortUrl}")
+    public String getLinkAddress(@PathVariable(name = "shortUrl") String shortUrl) {
+        return linkService.get(shortUrl);
+    }
+
+    @GetMapping(value = {"/{page}/{rowsPerPage}", "/"})
+    public List<Link> getLinks (@PathVariable(name = "page") Optional<Integer> page, @PathVariable(name = "rowsPerPage") Optional<Integer> rowsPerPage) {
+        if (page.isPresent() && rowsPerPage.isPresent()) {
+            return linkService.getLinks(page.get(), rowsPerPage.get());
+        } else {
+            return linkService.getLinks(1, 10);
+        }
+    }
+
+}
---
+++ src/main/java/com/hosmos/linkind/dao/LinkMapper.java
@@ -0,0 +1,34 @@
+package com.hosmos.linkind.dao;
+
+import com.hosmos.linkind.models.Link;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.dao.DuplicateKeyException;
+
+import java.util.List;
+
+public interface LinkMapper {
+    @Insert("INSERT INTO " +
+            "LINKS(ID, URL, OWNER, SHORT_URL) " +
+            "VALUES(nextval('SLINKS'), #{link.url}, #{link.owner}, #{link.shortUrl})")
+    void save(@Param("link") Link link) throws DuplicateKeyException;
+
+    @Select("SELECT * FROM LINKS WHERE ID = #{id}")
+    Link getWithId(@Param("id") int id);
+
+    @Select("SELECT URL FROM LINKS WHERE SHORT_URL = #{shortUrl}")
+    String getWithShortUrl(@Param("shortUrl") String shortUrl);
+
+    @Delete("DELETE FROM LINKS WHERE ID = #{id}")
+    int delete(@Param("id") int id);
+
+    @Select("SELECT" +
+            " ID," +
+            " URL," +
+            " SHORT_URL," +
+            " CREATION_DATE"+
+            " FROM LINKS ORDER BY CREATION_DATE LIMIT #{limit} OFFSET #{offset}")
+    List<Link> getLinks(@Param("limit") int limit, @Param("offset") int offset);
+}
